f19e79987a9062890c92092493eb3e4f4d5639ec
Carlos Pim <carlospimsilveira@gmail.com>
2025-01-27 20:53:53 -0300

    estudos do projeto novo

---
+++ Pro_01/assets/js/index.js
@@ -0,0 +1,26 @@
+function AdCalc(string) {
+    const display = document.getElementById('idisplay');
+    const operadores = ['+', '-', '*', '/']
+
+    if(operadores.includes(display.value.slice(-1)) && operadores.includes (string)) {
+        display.value = display.value.slice(0, -1) + string
+    } else {
+        display.value += string
+    }
+}
+    //string === 'Clear' ? display.value = '' : 
+
+function Calc() {
+    const display = document.getElementById('idisplay');
+
+    display.value = eval(display.value);
+}
+
+function Limp() {
+    const display = document.getElementById('idisplay')
+    let texto = display.value
+       
+    texto = texto.slice(0, -1)
+    display.value = texto;
+
+}
