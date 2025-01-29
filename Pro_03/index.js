570da13c0d07e4a1e0ba3fc7849056f71ebe582f
Carlos Pim <carlospimsilveira@gmail.com>
2025-01-29 20:03:25 -0300

    desconversor

--- Pro_03/index.js
+++ Pro_03/index.js
@@ -1,19 +1,39 @@
-function exibirTexto() {
-    const palavra = document.getElementById('userInput').value.toUpperCase();
-    const alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 
-                      'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 
-                      'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    
-    let resultado = [];
-  
-    for (let i = 0; i < palavra.length; i++) {
-      let posicao = alfabeto.indexOf(palavra[i]) + 1;
-      if (posicao > 0) {
-        resultado.push(posicao);
-      }
+function converter() {
+  const palavra = document.getElementById('userInput').value.toUpperCase();
+  const alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  let resultado = [];
+
+  for (let i = 0; i < palavra.length; i++) {
+    let posicao = alfabeto.indexOf(palavra[i]) + 1;
+    if (posicao > 0) {
+      resultado.push(posicao);
     }
-    const hist = document.getElementsByClassName('historico')[0];
-    hist.innerHTML = `<p>${resultado.join('-')}</p>`
-    console.log(resultado.join('-'));
+  }
+
+  const hist = document.getElementsByClassName('historico')[0];
+  hist.innerHTML = `<p>${resultado.join('-')}</p>`;
+  console.log(resultado.join('-'));
+  desconverter(resultado.join('-'));
 }
-  
+
+function desconverter(resultado) {
+  const alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  let numeros = resultado.split('-').map(Number);
+  let result = [];
+
+  for (let numero of numeros) {
+    if (numero >= 1 && numero <= 26) {
+      result.push(alfabeto[numero - 1]);
+    }
+  }
+
+  const hist = document.getElementsByClassName('historico')[0];
+  hist.innerHTML += `<p>${result.join('')}</p>`;
+  console.log(result.join(''));
+}
